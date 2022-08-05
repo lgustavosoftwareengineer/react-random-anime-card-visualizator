ffd604e1cdcfea648bee0afea121c8cb37ec19cd
Luiz Gustavo <luiz.matias@upe.br>
2022-08-05 16:34:19 -0300

    feat: create sign up user base page

--- src/features/modules/domains/sign-up-user/index.tsx
+++ src/features/modules/domains/sign-up-user/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const SignUpUser = () => {
+  const [userName, setUserName] = useState('')
+  const { push } = useRouter()
+
+  const isUserNameDefined = Boolean(userName.length)
+
+  const onClickGoToRandomDogsCardsPage = () => {
+    push('/cards')
+  }
+
   return (
     <div>
-      <p role="title">SignUpUser</p>
+      <input
+        type="text"
+        name="user-name-input"
+        placeholder="Digite aqui o seu nome"
+        value={userName}
+        onChange={({ target }) => setUserName(target.value)}
+      />
+      <input
+        type="button"
+        value="Clicar para ver cartas"
+        disabled={!isUserNameDefined}
+        onClick={onClickGoToRandomDogsCardsPage}
+      />
     </div>
   )
 }
