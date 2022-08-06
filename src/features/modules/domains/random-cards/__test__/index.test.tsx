bac22bbcc73c58b4b045c9e3f717db39910a79b1
Luiz Gustavo <luiz.matias@upe.br>
2022-08-06 03:03:56 -0300

    test: finish see random cards screen
    what change:
     - was create finish see random cards functionality
     - was configure next images domains
     - the random cards fetch was made fetch anime by id

--- src/features/modules/domains/random-cards/__test__/index.test.tsx
+++ src/features/modules/domains/random-cards/__test__/index.test.tsx
@@ -1,26 +1,102 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent, waitFor } from '@testing-library/react'
+import nock from 'nock'
+
 import { RandomCards } from '..'
 
+// import { fetchAnimeByIdResponseMock } from '../mocks'
+
+jest.mock('../utils', () => {
+  return {
+    buildCardsElementsListWithRandomNumbersValues: () => [1, 2, 3, 4, 5],
+  }
+})
+
+const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? ''
+
 describe('RandomCards', () => {
-  it('should show 5 spinners when cards is still loading', () => {
+  beforeEach(() => {
     render(<RandomCards />)
+  })
+
+  it('should show 5 spinners when cards is still loading', () => {
+    nock(NEXT_PUBLIC_API_BASE_URL)
+      .get(/anime\/[0-9].*/g)
+      .reply(200, {})
 
     const loadingIndicators = screen.getAllByTestId('loading-activity-test-id')
 
     expect(loadingIndicators).toHaveLength(5)
   })
-  it.todo('should show 5 when cards is still loaded with success')
-  it.todo(
-    "should add a new card in the end from the 5 cards when user click in 'Puxar uma nova carta aleatoriamente' button",
-  )
-  it.todo(
-    'should the new card added be shown with a spinner when it is still loading',
-  )
-  it.todo(
-    "should the 'Puxar uma nova carta aleatoriamente' button be disabled when user already clicked three times in it",
-  )
-  it.todo(
-    "should cards order be swapped when user click in 'Embaralhar cartas' button",
-  )
+
+  it('should show 5 when cards is still loaded with success', async () => {
+    await waitFor(async () => {
+      const cards = await screen.findAllByTestId('card-test-id')
+
+      expect(cards).toHaveLength(5)
+    })
+  })
+
+  it("should add a new card in the end from the 5 cards when user click in 'Puxar uma nova carta aleatoriamente' button", async () => {
+    const getMoreCardButton = screen.getByDisplayValue(
+      'Puxar uma nova carta aleatoriamente',
+    )
+
+    fireEvent.click(getMoreCardButton)
+
+    await waitFor(async () => {
+      const cards = await screen.findAllByTestId('card-test-id')
+      expect(cards).toHaveLength(6)
+    })
+  })
+  it('should the new card added be shown with a spinner when it is still loading', async () => {
+    nock(NEXT_PUBLIC_API_BASE_URL)
+      .get(/anime\/[0-9].*/g)
+      .reply(200, {})
+    const getMoreCardButton = screen.getByDisplayValue(
+      'Puxar uma nova carta aleatoriamente',
+    )
+
+    fireEvent.click(getMoreCardButton)
+
+    await waitFor(async () => {
+      const loadingIndicators = await screen.findAllByTestId(
+        'loading-activity-test-id',
+      )
+      expect(loadingIndicators).toHaveLength(1)
+    })
+  })
+  it("should the 'Puxar uma nova carta aleatoriamente' button be disabled when user already clicked three times in it", () => {
+    const getMoreCardButton = screen.getByDisplayValue(
+      'Puxar uma nova carta aleatoriamente',
+    )
+
+    fireEvent.click(getMoreCardButton)
+    fireEvent.click(getMoreCardButton)
+    fireEvent.click(getMoreCardButton)
+
+    expect(getMoreCardButton).toBeDisabled()
+  })
+
+  it("should the 'Puxar uma nova carta aleatoriamente' button display value be 'Limite atingido (3x)' when user already clicked three times in it", () => {
+    const getMoreCardButton = screen.getByDisplayValue(
+      'Puxar uma nova carta aleatoriamente',
+    )
+
+    fireEvent.click(getMoreCardButton)
+    fireEvent.click(getMoreCardButton)
+    fireEvent.click(getMoreCardButton)
+
+    expect(getMoreCardButton).toHaveDisplayValue('Limite atingido (3x)')
+  })
+
+  it("should cards order be swapped when user click in 'Embaralhar cartas' button", async () => {
+    const cardsBeforeShuffle = await screen.findAllByTestId('card-test-id')
+    const shuffleCardsButton = screen.getByDisplayValue('Embaralhar cartas')
+
+    fireEvent.click(shuffleCardsButton)
+    const cardsAfterShuffle = await screen.findAllByTestId('card-test-id')
+
+    expect(cardsBeforeShuffle).not.toEqual(cardsAfterShuffle)
+  })
 })
