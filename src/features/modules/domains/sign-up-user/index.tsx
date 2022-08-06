479df2bd1e0059a207d0487973aa7c86f14aebd5
Luiz Gustavo <luiz.matias@upe.br>
2022-08-06 14:09:13 -0300

    feat: create sign up user screen layout

--- src/features/modules/domains/sign-up-user/index.tsx
+++ src/features/modules/domains/sign-up-user/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useUser } from 'src/shared'
+import { Card } from 'src/components'
+
+import { CardContent, Main, TextInput, Button } from './styles'
 
 export const SignUpUser = () => {
   const [userName, setUserName] = useState('')
@@ -15,20 +18,28 @@ export const SignUpUser = () => {
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        name="user-name-input"
-        placeholder="Digite aqui o seu nome"
-        value={userName}
-        onChange={({ target }) => setUserName(target.value)}
-      />
-      <input
-        type="button"
-        value="Clicar para ver cartas"
-        disabled={!isUserNameDefined}
-        onClick={onClickGoToRandomCardsPage}
-      />
-    </div>
+    <Main>
+      <Card>
+        <CardContent>
+          <p>
+            Seja bem-vindo(a) ao <b>react-random-anime-card-visualizator</b>
+            <br /> Adicione seu nome logo a baixo para começar
+          </p>
+          <TextInput
+            type="text"
+            name="user-name-input"
+            placeholder="Digite aqui o seu nome"
+            value={userName}
+            onChange={({ target }) => setUserName(target.value)}
+          />
+          <Button
+            type="button"
+            value="Clicar para ver cartas"
+            disabled={!isUserNameDefined}
+            onClick={onClickGoToRandomCardsPage}
+          />
+        </CardContent>
+      </Card>
+    </Main>
   )
 }
---
+++ src/features/modules/domains/sign-up-user/styles.ts
@@ -0,0 +1,52 @@
+import styled, { css } from 'styled-components'
+
+export const Main = styled.main`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`
+export const CardContent = styled.div`
+  padding: 20px;
+  display: flex;
+  height: 50vh;
+  width: 50vw;
+  flex-direction: column;
+  justify-content: center;
+
+  input[type='text'] {
+    border: 1px solid #f3f3f3;
+  }
+`
+
+export const InputCSS = css`
+  margin-bottom: 20px;
+  border-radius: 6px;
+  padding: 20px;
+`
+
+export const TextInput = styled.input`
+  ${InputCSS};
+`
+
+export const Button = styled.input`
+  ${InputCSS};
+
+  background-color: #c20c33;
+  font-weight: bold;
+  color: #f3f3f3;
+  border: none;
+  transition: 0.3s;
+
+  &:hover,
+  &:enabled {
+    cursor: pointer;
+  }
+
+  &:disabled {
+    color: #f3f3f380;
+    background-color: #c20c3380;
+    cursor: auto;
+  }
+`
